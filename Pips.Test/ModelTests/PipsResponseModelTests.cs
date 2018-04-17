f6fc4d7312db1a6c222982a8b451855aaf7b2f1d
Sam Ehret <ehret.sam@yahoo.com>
2018-04-16 19:44:11 -0500

    added tests

---
+++ Pips.Test/ModelTests/PipsResponseModelTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Pips.Models;
+using Pips.Test.Extensions;
+using Xunit;
+
+namespace Pips.Test.ModelTests
+{
+    public class PipsResponseModelTests
+    {
+        readonly Type PipsResponseType = typeof(PipsResponse);
+
+        [Fact]
+        public void ValidatePipsResponseModelProperties()
+        {
+            Assert.Equal(2, PipsResponseType.GetProperties().Length);
+
+            Assert.True(PipsResponseType.HasPropertyOfType<string>("Request"));
+            Assert.True(PipsResponseType.HasPropertyOfType<IEnumerable<int>>("Rolls"));
+        }
+    }
+}
